af82e7803a85a27379ce107e501f59ffe93b8399
fluxrider <doompilot@gmail.com>
2022-08-13 13:53:23 -0400

    first test

--- vault.c
+++ vault.c
@@ -1,5 +1,8 @@
 // gcc --pedantic -Wall -Werror-implicit-function-declaration vault.c $(pkg-config --cflags --libs libsodium)
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <sys/uio.h>
 #include <sodium.h>
 
@@ -20,31 +23,47 @@ int main(int argc, char * argv[]) {
   {
     // encrypt
     unsigned char nonce[crypto_aead_xchacha20poly1305_ietf_NPUBBYTES]; randombytes_buf(nonce, crypto_aead_xchacha20poly1305_ietf_NPUBBYTES);
-    unsigned char src[src_n];
+    unsigned char * src = "username\npassword\nurl\nnote1\nnote2"; size_t src_n = strlen(src);
     unsigned char dst[src_n+crypto_aead_xchacha20poly1305_ietf_ABYTES];
-    unsigned long long dst_n; crypto_aead_xchacha20poly1305_ietf_encrypt(dst, &dst_n, src, src_n, NULL, 0, NULL, nonce, key);
-    // write encrypted file (kdf_algo, kdf_algo_p1, kdf_algo_p2, kdf_salt, encryption_nonce, encrypted_message)
-    struct iovec iov[6];
+    unsigned long long _dst_n; crypto_aead_xchacha20poly1305_ietf_encrypt(dst, &_dst_n, src, src_n, NULL, 0, NULL, nonce, key);
+    uint64_t encrypted_n = _dst_n;
+    // write encrypted file (kdf_algo, kdf_algo_p1, kdf_algo_p2, kdf_salt, encryption_nonce, encrypted_len, encrypted_message)
+    struct iovec iov[7];
     iov[0].iov_base = &algo; iov[0].iov_len = sizeof(uint64_t);
     iov[1].iov_base = &algo_p1; iov[1].iov_len = sizeof(uint64_t);
     iov[2].iov_base = &algo_p2; iov[2].iov_len = sizeof(uint64_t);
-    iov[0].iov_base = salt; iov[0].iov_len = crypto_pwhash_SALTBYTES;
-    iov[0].iov_base = nonce; iov[0].iov_len = crypto_aead_xchacha20poly1305_ietf_NPUBBYTES;
-    iov[0].iov_base = dst; iov[0].iov_len = dst_n;
-    int fd = E_CHK(open("out.encrypted", O_RDONLY));
-    E_CHK(writev(fd, iov, 6));
+    iov[3].iov_base = salt; iov[3].iov_len = crypto_pwhash_SALTBYTES;
+    iov[4].iov_base = nonce; iov[4].iov_len = crypto_aead_xchacha20poly1305_ietf_NPUBBYTES;
+    iov[5].iov_base = &encrypted_n; iov[5].iov_len = sizeof(uint64_t);
+    iov[6].iov_base = dst; iov[6].iov_len = encrypted_n;
+    int fd = E_CHK(open("entry.encrypted", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR ));
+    E_CHK(writev(fd, iov, 7));
     E_CHK(close(fd));
   }
 
   // decrypt
   {
-    unsigned char src[n];
-    unsigned char dst[n-crypto_secretstream_xchacha20poly1305_HEADERBYTES-crypto_secretstream_xchacha20poly1305_ABYTES];
-    crypto_secretstream_xchacha20poly1305_state st;
-    if(crypto_secretstream_xchacha20poly1305_init_pull(&st, src, key)) { fprintf(stderr, "ERROR: crypto_secretstream_xchacha20poly1305_init_pull()\n"); return EXIT_FAILURE; };
-    unsigned char tag; if(crypto_secretstream_xchacha20poly1305_pull(&st, dst, NULL, &tag, src+crypto_secretstream_xchacha20poly1305_HEADERBYTES, n-crypto_secretstream_xchacha20poly1305_HEADERBYTES, NULL, 0)) { fprintf(stderr, "ERROR: crypto_secretstream_xchacha20poly1305_pull()\n"); return EXIT_FAILURE; };
-    if(tag != crypto_secretstream_xchacha20poly1305_TAG_FINAL) { fprintf(stderr, "ERROR: unexpected tag\n"); return EXIT_FAILURE; };
-    
+    algo = algo_p1 = algo_p2 = 0; memset(salt, 0, crypto_pwhash_SALTBYTES);
+    unsigned char nonce[crypto_aead_xchacha20poly1305_ietf_NPUBBYTES];
+    uint64_t encrypted_n;
+    int fd = E_CHK(open("entry.encrypted", O_RDONLY));
+    struct iovec iov[6];
+    iov[0].iov_base = &algo; iov[0].iov_len = sizeof(uint64_t);
+    iov[1].iov_base = &algo_p1; iov[1].iov_len = sizeof(uint64_t);
+    iov[2].iov_base = &algo_p2; iov[2].iov_len = sizeof(uint64_t);
+    iov[3].iov_base = salt; iov[3].iov_len = crypto_pwhash_SALTBYTES;
+    iov[4].iov_base = nonce; iov[4].iov_len = crypto_aead_xchacha20poly1305_ietf_NPUBBYTES;
+    iov[5].iov_base = &encrypted_n; iov[5].iov_len = sizeof(uint64_t);
+    if(encrypted_n <= crypto_aead_xchacha20poly1305_ietf_ABYTES) { fprintf(stderr, "ERROR: encrypted length too short\n"); return EXIT_FAILURE; }
+    E_CHK(readv(fd, iov, 6));
+    unsigned char encrypted[encrypted_n];
+    ssize_t n = E_CHK(read(fd, encrypted, encrypted_n)); if(n != encrypted_n) { fprintf(stderr, "ERROR: read()\n"); return EXIT_FAILURE; }
+    unsigned char buffer[encrypted_n - crypto_aead_xchacha20poly1305_ietf_ABYTES + 1]; // +1 tmp, null terminate
+    unsigned long long buffer_n;
+    if(crypto_aead_xchacha20poly1305_ietf_decrypt(buffer, &buffer_n, NULL, encrypted, encrypted_n, NULL, 0, nonce, key)) { fprintf(stderr, "ERROR: crypto_aead_xchacha20poly1305_ietf_decrypt()\n"); return EXIT_FAILURE; }
+    E_CHK(close(fd));
+    buffer[buffer_n] = '\0'; // tmp
+    printf("Decrypted %s\n", buffer);
   }
   
   return EXIT_SUCCESS;
