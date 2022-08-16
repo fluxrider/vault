53a5b1bfe800e4f967db894ab4996138c86e0541
fluxrider <doompilot@gmail.com>
2022-08-16 15:30:37 -0400

    frame the textview so it has a border

--- gtk.c
+++ gtk.c
@@ -27,13 +27,12 @@ static void on_activate(GtkApplication * app) {
   gtk_box_append(GTK_BOX(password_row), password);
 
   GtkWidget * notes = gtk_text_view_new(); gtk_widget_set_hexpand(notes, true); gtk_widget_set_vexpand(notes, true);
-  GtkWidget * notes_label = gtk_label_new("Notes: ");
-  GtkWidget * notes_scroll = gtk_scrolled_window_new();
-  gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(notes_scroll), notes);
-  GtkWidget * notes_row = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
-  gtk_widget_set_valign(notes_label, GTK_ALIGN_START);
+  GtkWidget * notes_scroll = gtk_scrolled_window_new(); gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(notes_scroll), notes);
+  GtkWidget * notes_frame = gtk_frame_new(NULL); gtk_frame_set_child(GTK_FRAME(notes_frame), notes_scroll);  
+  GtkWidget * notes_label = gtk_label_new("Notes: "); gtk_widget_set_valign(notes_label, GTK_ALIGN_START);
+  GtkWidget * notes_row = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);  
   gtk_box_append(GTK_BOX(notes_row), notes_label);
-  gtk_box_append(GTK_BOX(notes_row), notes_scroll);
+  gtk_box_append(GTK_BOX(notes_row), notes_frame);
 
   GtkWidget * vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 3);
   gtk_box_append(GTK_BOX(vbox), entry_row);
