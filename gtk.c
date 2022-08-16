a6a120d177ee13b5ca15f411ac559c9753245343
fluxrider <doompilot@gmail.com>
2022-08-16 14:43:01 -0400

    (ongoing) gtk expand

--- gtk.c
+++ gtk.c
@@ -6,25 +6,27 @@ static void on_activate(GtkApplication * app) {
 //  g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_window_close), window);
 //  gtk_window_set_child(GTK_WINDOW (window), button);
 
+  GtkWidget * entry = gtk_entry_new(); gtk_widget_set_hexpand(entry, true);
   GtkWidget * entry_row = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
   gtk_box_append(GTK_BOX(entry_row), gtk_label_new("Entry: "));
-  gtk_box_append(GTK_BOX(entry_row), gtk_entry_new());
+  gtk_box_append(GTK_BOX(entry_row), entry);
 
+  GtkWidget * url = gtk_entry_new(); gtk_widget_set_hexpand(url, true);
   GtkWidget * url_row = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
   gtk_box_append(GTK_BOX(url_row), gtk_label_new("URL: "));
-  gtk_box_append(GTK_BOX(url_row), gtk_entry_new());
+  gtk_box_append(GTK_BOX(url_row), url);
 
-  GtkWidget * username = gtk_password_entry_new(); gtk_password_entry_set_show_peek_icon(GTK_PASSWORD_ENTRY(username), true);
+  GtkWidget * username = gtk_password_entry_new(); gtk_widget_set_hexpand(username, true); gtk_password_entry_set_show_peek_icon(GTK_PASSWORD_ENTRY(username), true);
   GtkWidget * username_row = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
   gtk_box_append(GTK_BOX(username_row), gtk_label_new("Username: "));
   gtk_box_append(GTK_BOX(username_row), username);
 
-  GtkWidget * password = gtk_password_entry_new(); gtk_password_entry_set_show_peek_icon(GTK_PASSWORD_ENTRY(password), true);
+  GtkWidget * password = gtk_password_entry_new(); gtk_widget_set_hexpand(password, true); gtk_password_entry_set_show_peek_icon(GTK_PASSWORD_ENTRY(password), true);
   GtkWidget * password_row = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
   gtk_box_append(GTK_BOX(password_row), gtk_label_new("Password: "));
   gtk_box_append(GTK_BOX(password_row), password);
 
-  GtkWidget * notes = gtk_text_view_new();
+  GtkWidget * notes = gtk_text_view_new(); gtk_widget_set_hexpand(notes, true); gtk_widget_set_vexpand(notes, true);
   GtkWidget * notes_scroll = gtk_scrolled_window_new();
   gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(notes_scroll), notes);
   GtkWidget * notes_row = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
