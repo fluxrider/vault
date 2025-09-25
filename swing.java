2933f34772b929ee55d12295f232be95faf4cce8
fluxrider <doompilot@gmail.com>
2025-09-25 18:50:39 -0400

    copy to clipboard

--- swing.java
+++ swing.java
@@ -7,44 +7,48 @@
 import javax.swing.*;
 import java.util.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class swing {
+
+  static JTextField id;
+  static JTextField url;
+  static JTextField username;
+  static JTextField password;
+  static JTextArea notes;
+  
+  static void clipboard(String s) { Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new java.awt.datatransfer.StringSelection(s), null); }
+  
   public static void main(String[] args) {
+    JTextField id = new JTextField();
+    JTextField url = new JTextField();
+    JTextField username = new JTextField();
+    JTextField password = new JTextField();
+    JTextArea notes = new JTextArea();
     JFrame frame = new JFrame("Vault"); //frame.setIconImage(UIManager.getIcon("FileView.directoryIcon").getImage());
     frame.setContentPane(new JPanel(new BorderLayout()) {{
       this.add(new JPanel() {{
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        this.add(new JPanel() {{
-          this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-          this.add(new JLabel("ID"));
-          this.add(new JTextField());
+        this.add(new JPanel() {{ this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+          this.add(new JLabel("ID")); this.add(id);
         }});
-        this.add(new JPanel() {{
-          this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-          this.add(new JLabel("URL", UIManager.getIcon("FileChooser.homeFolderIcon"), JLabel.LEFT));
-          this.add(new JTextField());
-          this.add(new JButton("Copy", UIManager.getIcon("FileView.floppyDriveIcon")));
+        this.add(new JPanel() {{ this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+          this.add(new JLabel("URL", UIManager.getIcon("FileChooser.homeFolderIcon"), JLabel.LEFT)); this.add(url);
+          JButton copy = new JButton("Copy", UIManager.getIcon("FileView.floppyDriveIcon")); copy.addActionListener(new ActionListener() { public void actionPerformed(ActionEvent e) { clipboard(url.getText()); }}); this.add(copy);
         }});
-        this.add(new JPanel() {{
-          this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-          this.add(new JLabel("Username"));
-          this.add(new JTextField());
-          this.add(new JButton("Copy", UIManager.getIcon("FileView.floppyDriveIcon")));
+        this.add(new JPanel() {{ this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+          this.add(new JLabel("Username")); this.add(username);
+          JButton copy = new JButton("Copy", UIManager.getIcon("FileView.floppyDriveIcon")); copy.addActionListener(new ActionListener() { public void actionPerformed(ActionEvent e) { clipboard(username.getText()); }}); this.add(copy);
         }});
-        this.add(new JPanel() {{
-          this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-          this.add(new JLabel("Password"));
-          this.add(new JTextField());
-          this.add(new JButton("Copy", UIManager.getIcon("FileView.floppyDriveIcon")));
+        this.add(new JPanel() {{ this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+          this.add(new JLabel("Password")); this.add(password);
+          JButton copy = new JButton("Copy", UIManager.getIcon("FileView.floppyDriveIcon")); copy.addActionListener(new ActionListener() { public void actionPerformed(ActionEvent e) { clipboard(password.getText()); }}); this.add(copy);
         }});
-        this.add(new JPanel() {{
-          this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-          this.add(new JLabel("Notes"));
-          this.add(new JTextArea());
+        this.add(new JPanel() {{ this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+          this.add(notes); this.add(new JTextArea());
         }});
       }}, BorderLayout.CENTER);
-      this.add(new JPanel() {{
-        this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+      this.add(new JPanel() {{ this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
         this.add(new JButton("Save", UIManager.getIcon("FileView.floppyDriveIcon")));
         this.add(new JButton("Open", UIManager.getIcon("FileView.directoryIcon")));
       }}, BorderLayout.SOUTH);
