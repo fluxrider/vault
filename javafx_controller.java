593c0f5e371b0ce71cf8874e9f6fe43890dfca7f
fluxrider <doompilot@gmail.com>
2025-09-27 13:07:33 -0400

    using skeleton controller

---
+++ javafx_controller.java
@@ -0,0 +1,73 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class javafx_controller {
+
+    @FXML
+    private TextField id;
+
+    @FXML
+    private TextArea notes;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField password_viz;
+
+    @FXML
+    private ImageView password_viz_icon;
+
+    @FXML
+    private TextField url;
+
+    @FXML
+    private PasswordField username;
+
+    @FXML
+    private TextField username_viz;
+
+    @FXML
+    private ImageView username_viz_icon;
+
+    @FXML
+    void on_copy_password(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_copy_url(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_copy_username(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_open(ActionEvent event) {
+        System.out.println("on open");
+    }
+
+    @FXML
+    void on_password_viz(MouseEvent event) {
+
+    }
+
+    @FXML
+    void on_save(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_username_viz(MouseEvent event) {
+
+    }
+
+}
