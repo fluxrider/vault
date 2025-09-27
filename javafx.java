dd4cb611e5d6dfeb0b419fef12c9413a48492919
fluxrider <doompilot@gmail.com>
2025-09-27 13:26:21 -0400

    (ongoing) working on javafx, this might not compile, backing up

--- javafx.java
+++ javafx.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
+import javafx.scene.image.*;
 import javafx.fxml.*;
 import java.io.*;
 
@@ -20,9 +21,14 @@ public class javafx extends Application { public static void main(String[] args)
     stage.setScene(scene);
     stage.show();
     */
+    Image hide_icon = new Image("file:password-hide.png");
+    Image show_icon = new Image("file:password-show.png");
     
     FXMLLoader loader = new FXMLLoader(new File("javafx.fxml").toURI().toURL());
     Parent root = loader.load();
+    javafx_controller controller = loader.<javafx_controller>getController();
+    controller.set_icons(hide_icon, show_icon);
+    
     Scene scene = new Scene(root);
     stage.setTitle("Vault");
     stage.setScene(scene);
--- javafx_controller.java
+++ javafx_controller.java
@@ -3,71 +3,61 @@ import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
+import javafx.scene.image.*;
 import javafx.scene.input.MouseEvent;
 
 public class javafx_controller {
 
-    @FXML
-    private TextField id;
+  private Image hide_icon, show_icon;
+  
+  void set_icons(Image hide_icon, Image show_icon) {
+    this.hide_icon = hide_icon;
+    this.show_icon = show_icon;
+    this.username_viz_icon.setImage(hide_icon);
+    this.password_viz_icon.setImage(hide_icon);
+    // toggle to hidden
+    textField.toFront();
+    imageView.toFront();
+  }
 
-    @FXML
-    private TextArea notes;
+  @FXML private TextField id;
 
-    @FXML
-    private PasswordField password;
+  @FXML private TextArea notes;
 
-    @FXML
-    private TextField password_viz;
+  @FXML private PasswordField password;
 
-    @FXML
-    private ImageView password_viz_icon;
+  @FXML private TextField password_viz;
 
-    @FXML
-    private TextField url;
+  @FXML private ImageView password_viz_icon;
 
-    @FXML
-    private PasswordField username;
+  @FXML private TextField url;
 
-    @FXML
-    private TextField username_viz;
+  @FXML private PasswordField username;
 
-    @FXML
-    private ImageView username_viz_icon;
+  @FXML private TextField username_viz;
 
-    @FXML
-    void on_copy_password(ActionEvent event) {
+  @FXML private ImageView username_viz_icon;
+  
+  @FXML void on_copy_password(ActionEvent event) {
+  }
 
-    }
+  @FXML void on_copy_url(ActionEvent event) {
+  }
 
-    @FXML
-    void on_copy_url(ActionEvent event) {
+  @FXML void on_copy_username(ActionEvent event) {
+  }
 
-    }
+  @FXML void on_open(ActionEvent event) {
+    System.out.println("on open");
+  }
 
-    @FXML
-    void on_copy_username(ActionEvent event) {
+  @FXML void on_password_viz(MouseEvent event) {
+  }
 
-    }
+  @FXML void on_save(ActionEvent event) {
+  }
 
-    @FXML
-    void on_open(ActionEvent event) {
-        System.out.println("on open");
-    }
-
-    @FXML
-    void on_password_viz(MouseEvent event) {
-
-    }
-
-    @FXML
-    void on_save(ActionEvent event) {
-
-    }
-
-    @FXML
-    void on_username_viz(MouseEvent event) {
-
-    }
+  @FXML void on_username_viz(MouseEvent event) {
+  }
 
 }
