cb54e91d625f6c628dd7ff8b049beee046968935
fluxrider <doompilot@gmail.com>
2025-09-27 14:57:14 -0400

    password field toggle view

--- javafx.java
+++ javafx.java
@@ -1,5 +1,5 @@
 // Copyright 2025 David Lareau. This source code form is subject to the terms of the Mozilla Public License 2.0.
-// javac --module-path /var/lib/snapd/snap/openjfx/current/sdk/lib --add-modules javafx.controls,javafx.fxml javafx.java -d java_out
+// javac --module-path /var/lib/snapd/snap/openjfx/current/sdk/lib --add-modules javafx.controls,javafx.fxml javafx*.java -d java_out
 // java --module-path /var/lib/snapd/snap/openjfx/current/sdk/lib --add-modules javafx.controls,javafx.fxml --enable-native-access=javafx.graphics -Dglass.gtk.uiScale=1.5 -cp java_out javafx
 // note: openjfx was a pain to install. I ended up install snapd from AUR, then sudo systemctl start snapd, then sudo snap install openjfx
 
--- javafx_controller.java
+++ javafx_controller.java
@@ -8,36 +8,12 @@ import javafx.scene.input.MouseEvent;
 
 public class javafx_controller {
 
-  private Image hide_icon, show_icon;
-  
-  void set_icons(Image hide_icon, Image show_icon) {
-    this.hide_icon = hide_icon;
-    this.show_icon = show_icon;
-    this.username_viz_icon.setImage(hide_icon);
-    this.password_viz_icon.setImage(hide_icon);
-    // toggle to hidden
-    textField.toFront();
-    imageView.toFront();
-  }
-
   @FXML private TextField id;
-
+  @FXML private TextField url;
+  @FXML private PasswordField password; @FXML private TextField password_viz; @FXML private ImageView password_viz_icon;
+  @FXML private PasswordField username; @FXML private TextField username_viz; @FXML private ImageView username_viz_icon;
   @FXML private TextArea notes;
 
-  @FXML private PasswordField password;
-
-  @FXML private TextField password_viz;
-
-  @FXML private ImageView password_viz_icon;
-
-  @FXML private TextField url;
-
-  @FXML private PasswordField username;
-
-  @FXML private TextField username_viz;
-
-  @FXML private ImageView username_viz_icon;
-  
   @FXML void on_copy_password(ActionEvent event) {
   }
 
@@ -51,13 +27,47 @@ public class javafx_controller {
     System.out.println("on open");
   }
 
-  @FXML void on_password_viz(MouseEvent event) {
-  }
-
   @FXML void on_save(ActionEvent event) {
   }
 
+  private Image hide_icon, show_icon;
+  void set_icons(Image hide_icon, Image show_icon) {
+    this.hide_icon = hide_icon;
+    this.show_icon = show_icon;
+    this.password_viz_icon.setImage(hide_icon);
+    this.username_viz_icon.setImage(hide_icon);
+    // set to hidden
+    this.password.toFront();
+    this.password_viz_icon.toFront();
+    this.username.toFront();
+    this.username_viz_icon.toFront();
+    // ensure both field are in sync
+    this.password.textProperty().bindBidirectional(this.password_viz.textProperty());
+    this.username.textProperty().bindBidirectional(this.username_viz.textProperty());
+  }
+  @FXML void on_password_viz(MouseEvent event) {
+    boolean reveal = this.password_viz_icon.getImage() == this.hide_icon;
+    if(reveal) {
+      this.password_viz.toFront();
+      this.password_viz_icon.toFront();
+      this.password_viz_icon.setImage(this.show_icon);
+    } else {
+      this.password.toFront();
+      this.password_viz_icon.toFront();
+      this.password_viz_icon.setImage(this.hide_icon);
+    }
+  }
   @FXML void on_username_viz(MouseEvent event) {
+    boolean reveal = this.username_viz_icon.getImage() == this.hide_icon;
+    if(reveal) {
+      this.username_viz.toFront();
+      this.username_viz_icon.toFront();
+      this.username_viz_icon.setImage(this.show_icon);
+    } else {
+      this.username.toFront();
+      this.username_viz_icon.toFront();
+      this.username_viz_icon.setImage(this.hide_icon);
+    }
   }
 
 }
